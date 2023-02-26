8fd6b4249a0cd1d6c473e0940ac960fcc7e07fd6
Abed Mohsen <114285253+AbedMohsen@users.noreply.github.com>
2023-02-26 14:59:38 +0200

    Update languages.js

--- src/components/languages.js
+++ src/components/languages.js
@@ -24,15 +24,24 @@ function Languages({ language, onLanguageChange }) {
 const LANGUAGES = [
   { label: "Afrikaans", value: "af" },
   { label: "Arabic", value: "ar" },
+  { label: "Dutch", value: "nl" },
   { label: "French", value: "fr" },
+  { label: "German", value: "de" },
+  { label: "Greek", value: "el" },
+  { label: "Hebrew", value: "iw" },
   { label: "Hindi", value: "hi" },
+  { label: "Italian", value: "it" },
   { label: "Japanese", value: "ja" },
+  { label: "Korean", value: "ko" },
+  { label: "Latin", value: "la" },
+  { label: "Persian", value: "fa" },
   { label: "Portuguese", value: "pt" },
   { label: "Russian", value: "ru" },
   { label: "Simplified Chinese", value: "zh-CN" },
   { label: "Spanish", value: "es" },
   { label: "Swahili", value: "sw" },
-  { label: "Thai", value: "th" }
+  { label: "Thai", value: "th" },
+  { label: "Turkish", value: "tr" }
 ];
 
 export default Languages;
